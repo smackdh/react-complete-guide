c57b531cde99ee57e7d055621b6ba37f19ec90d0
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-15 16:46:16 +0900

    added css to ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
-function ExpenseItem() {
-  return <h2>This is component!</h2>;
-}
+import "./ExpenseItem.css";
 
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>December 15 2022</div>
+      <div className="expense-item__description">
+        <h2>Cat Food</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
+}
 export default ExpenseItem;
