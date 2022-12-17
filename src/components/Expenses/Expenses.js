646db532182e596ed69c6a1aa0037b129b658de1
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-17 16:39:23 +0900

    added logic to iterate over expenses array

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,23 +17,13 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <div>
+      {props.items.map((expense) => (
         <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-      </div>
+      ))}
     </Card>
   );
 };
